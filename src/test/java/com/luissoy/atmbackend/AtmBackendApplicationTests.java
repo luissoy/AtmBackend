309f5215be0303d3893a3ac36b91d3936ecbdaa9
luissoy <luisserranomarin@gmail.com>
2024-02-10 18:52:30 +0100

    feat: Project start

---
+++ src/test/java/com/luissoy/atmbackend/AtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissoy.atmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
